2d3590c6d5a6e7d976c6b0df7f4386e916f5bc29
Articrox <57105382+Articrox@users.noreply.github.com>
2019-12-06 08:58:21 -0500

    Update main.c
    added the function for the power of another number

--- main.c
+++ main.c
@@ -110,7 +110,19 @@ void SquareRoot (void){
 }
 //Program for PowerOfB
 void PowerOfB (void){
-     printf("\n\rYou have selected Squareroot");
+     
+    int base, exp;
+    long long result = 1;
+    printf("Enter a base number: ");
+    scanf("%d", &base);
+    printf("Enter an exponent: ");
+    scanf("%d", &exp);
+    while (exp != 0) {
+        result *= base;
+        --exp;
+    }
+    printf("Answer = %lld", result);
+    return 0;
 }
 //Program for factorial
 void factorial (void){
